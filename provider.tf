1f110e5860d5ec79f610326db925f49fef56fe61
prasad sripathi <prasad18218@gmail.com>
2025-11-05 08:38:41 +0530

    terraform vpc

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+
+  backend "s3" {
+
+    bucket = "spd-remote-state"
+    key = "ec2-module-demo"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+    
+  }
+}
+
+provider "aws" {
+
+       region = "us-east-1"
+}
